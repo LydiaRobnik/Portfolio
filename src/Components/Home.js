ddcc092b347d660600afc511c8e07608d2a7ca0f
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-06-01 20:34:46 +0200

    profile pic added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Image, Box } from '@chakra-ui/react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Box>
+        <Image
+          src="profile-pic-ly.jpg"
+          alt="Profile picture Lydia Robnik"
+          borderRadius="full"
+        />
+      </Box>
+    </>
+  );
 };
 
 export default Home;
