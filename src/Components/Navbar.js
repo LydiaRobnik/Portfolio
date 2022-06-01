d5a38f844528522d58ef8a6c1381daa079aef4be
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-06-01 20:21:39 +0200

    mobile navbar added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,28 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, IconButton } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState('none');
+
+  const handleClick = () => {
+    setIsOpen(isOpen === 'none' ? 'block' : 'none');
+  };
+
   return (
-    <div>
+    <>
       <nav>
-        <Flex>
-          <Flex>
+        {/* navbar screen size > lg */}
+        <Flex pos="fixed" top="1rem" right="1rem" align="center">
+          <Flex display={['none', 'none', 'flex', 'flex']}>
             <NavLink to="/">
-              <Button>Home</Button>
+              <Button
+                as="a"
+                variant="ghost"
+                aria-lable="Home"
+                my={5}
+                w="100%"
+                cursor="pointer"
+              >
+                Home
+              </Button>
             </NavLink>
             <NavLink to="/about">
-              <Button>About</Button>
+              <Button as="a" variant="ghost" aria-lable="About" my={5} w="100%">
+                About
+              </Button>
             </NavLink>
             <NavLink to="/contact">
-              <Button>Contact</Button>
+              <Button
+                as="a"
+                variant="ghost"
+                aria-lable="Contact"
+                my={5}
+                w="100%"
+              >
+                Contact
+              </Button>
             </NavLink>
           </Flex>
+          <IconButton
+            aria-label="Open Menu"
+            size="lg"
+            mr={2}
+            icon={<HamburgerIcon />}
+            display={['flex', 'flex', 'none', 'none']}
+            onClick={handleClick}
+          />
           <ColorModeSwitcher justifySelf="flex-end" />
         </Flex>
+        {/* mobile menue, screen size < lg */}
+        <Flex
+          w="100vw"
+          bgColor="gray.50"
+          zIndex={20}
+          h="100vh"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflow="auto"
+          direction="column"
+          display={isOpen}
+        >
+          <Flex justify="flex-end">
+            <IconButton
+              aria-label="Close Menu"
+              size="lg"
+              mt={2}
+              mr={2}
+              icon={<CloseIcon />}
+              onClick={handleClick}
+            />
+          </Flex>
+
+          <Flex direction="column" align="center">
+            <NavLink to="/">
+              <Button as="a" variant="ghost" aria-lable="Home" my={5} w="100%">
+                Home
+              </Button>
+            </NavLink>
+            <NavLink to="/about">
+              <Button as="a" variant="ghost" aria-lable="About" my={5} w="100%">
+                About
+              </Button>
+            </NavLink>
+            <NavLink to="/contact">
+              <Button
+                as="a"
+                variant="ghost"
+                aria-lable="Contact"
+                my={5}
+                w="100%"
+              >
+                Contact
+              </Button>
+            </NavLink>
+          </Flex>
+        </Flex>
       </nav>
-    </div>
+    </>
   );
 };
 
